85e101c05738e16fe2681a7fe9e148c9e6b6f9ab
PROxZIMA <pratikbpingale9075@gmail.com>
2022-07-18 14:03:11 +0530

    Refactor: Improved mouse id selection

--- enable_autoscrolling.sh
+++ enable_autoscrolling.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-ID=$(xinput list | grep -Po 'Logitech Wireless Receiver Mouse\s+id=\K(\d+)');
+ID=$(xinput list | grep -Po 'Mouse\s+id=\K(\d+)');
 xinput set-prop $ID "libinput Scroll Method Enabled" 0, 0, 1;
 xinput set-prop $ID "libinput Button Scrolling Button" 2;
 echo "xinput set-prop ${ID} 'libinput Scroll Method Enabled' 0, 0, 1\nxinput set-prop ${ID} 'libinput Button Scrolling Button' 2" > ~/.xsessionrc;
